b0554bebbae4fda811a66b179743a95cdc15ba73
ViktorKernytskyi <viktorkernytskyi@gmail.com>
2022-01-28 19:53:48 +0200

    cart

--- add.php
+++ add.php
@@ -1,1 +1,24 @@
 <?php
+session_start();
+
+include('cart_1.php');
+?>
+
+<a href="/cart/list.php">Список </a>
+
+<form method="post" action="/add.php">
+    <select name="product">
+        <?php
+
+                foreach ($products as $id => $product){
+            echo '<option value="' . $id .'">' . $product['name'] . '</option>';
+        }
+        ?>
+
+    </select>
+    
+    <input type="number" name="count" value="">
+    <input hidden name="action" value="add">
+
+    <button type="submit" >Отправить</button>
+</form>
